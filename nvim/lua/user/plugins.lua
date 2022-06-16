084f551a2cc165e32a288470fbfbad481d2a9762
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-06-16 09:18:02 +0200

    clean + better dashboard

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,11 +1,17 @@
-local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    packer_bootstrap = vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
 end
 
 return require("packer").startup(function()
     use "wbthomason/packer.nvim"
+    use {
+        'goolord/alpha-nvim',
+        config = function ()
+            require'alpha'.setup(require'alpha.themes.dashboard'.config)
+        end
+    }
+
     use "onsails/lspkind.nvim"
     use "github/copilot.vim"
     use "tpope/vim-sensible"
@@ -27,7 +33,7 @@ return require("packer").startup(function()
     use "tamago324/nlsp-settings.nvim"
     use "jose-elias-alvarez/null-ls.nvim"
     --use "windwp/nvim-autopairs"
-    use "glepnir/dashboard-nvim"
+    --use "glepnir/dashboard-nvim"
     use "Chiel92/vim-autoformat"
     use { "neovim/nvim-lspconfig", "williamboman/nvim-lsp-installer" }
     use { "folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons", }
