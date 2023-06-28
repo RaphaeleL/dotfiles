edc6e61bf8d3090e5b017776665ca5a8cc5da817
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-06-28 12:28:46 +0200

    Update Alias'

--- shell/alias.sh
+++ shell/alias.sh
@@ -1,9 +1,11 @@
 # Basics 
 alias lg="lazygit"
 alias tldrf="tldr --list | fzf --preview 'tldr {1} --color=always' --preview-window=right,70% | xargs tldr"
-alias rgrep="rg"
 alias vim="nvim"
-alias vi="/usr/bin/vim"
+alias vi="NVIM_APPNAME=macnvim nvim"
+alias v="/usr/bin/vim"
+alias e="emacs -nw"
+alias rgrep="rg"
 
 # Git
 alias gs="git status"
@@ -21,6 +23,7 @@ alias .4="cd ../../../.."
 alias .5="cd ../../../../.."
 
 # Tmux
+alias tn="tmux display-message -p '#S'"
 alias tl="tmux list-sessions"
 alias ta="tmux attach -t"
 alias td="tmux detach"
@@ -45,3 +48,29 @@ tms() {
 
 # Permissions 
 alias perms="stat -f '%N %A' *"
+
+# Easter Eggs
+alias bitcoin="open /System/Library/Image\ Capture/Devices/VirtualScanner.app/Contents/Resources/simpledoc.pdf"
+function theme() {
+    osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to not dark mode'
+    DARK_MODE=$(osascript -e 'tell app "System Events" to tell appearance preferences to get dark mode')
+    if [[ "${DARK_MODE}" == "true" ]]; then
+        export PS1='\[\e[01;32m\]raphaele@\h:\[\e[01;34m\]\w\[\e[0m\]\$ '
+    else
+        export PS1='\h:\W \u\$ '
+    fi
+}
+
+# Spezial Commands simplified 
+alias remove="shred -n 512 --remove "
+alias sizes="du -sh * | gsort -hr"
+
+# List Directory
+alias ls='ls -Ghp'
+
+# List Directory - exa
+alias la="exa -a --color=always --group-directories-first" 
+# alias ll="exa -aril --color=always --group-directories-first"  
+# alias ls="exa --color=always --group-directories-first"  
+# alias lt="exa -aT --color=always --group-directories-first" 
+# alias l.="exa -a | egrep "^\.""
--- shell/alias_exa.sh
+++
@@ -1,5 +0,0 @@
-alias la="exa -a --color=always --group-directories-first" 
-alias ll="exa -aril --color=always --group-directories-first"  
-alias ls="exa --color=always --group-directories-first"  
-alias lt="exa -aT --color=always --group-directories-first" 
-alias l.="exa -a | egrep "^\.""
