56d9d34c7b085a9bb26dd032aba11827fbe055db
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-04-30 13:55:02 +0200

    Updates

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -16,11 +16,7 @@ config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
 config.tab_max_width = 22
 
-<<<<<<< HEAD
-config.hide_tab_bar_if_only_one_tab = true 
-=======
 config.hide_tab_bar_if_only_one_tab = true
->>>>>>> 2a8ca77d (Updates)
 
 config.window_padding = {
 	left = 10,
